e92de220665d77a16a2ba5896e44cd947fe48cb1
yahia-work <yahiawork35@gmail.com>
2024-10-11 13:29:19 +0100

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom'; // Outlet pour rendre le contenu des sous-routes
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import { PanierContext } from './Context/PanierContext';
 
 function App() {
+  const [Produits,setproducts]= useState([])
+  const handelAdd = (p) => setproducts([...Produits,p]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        hello world i am othmani sadek
-      </header>
-    </div>
+    <PanierContext.Provider value={{Produits,handelAdd}}>
+      <div className="App">
+        <Header />
+        <div className="urgent flex">
+                Livraison disponible 58 wilaya
+        </div>
+        <Outlet /> 
+        <Footer />
+      </div>
+    </PanierContext.Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Acceuil from './Acceuil/Acceuil';
+import Boutique from './Boutique/Boutique';
+import Apropre from './Aproprs/Apropre';
+import Faq from './Faq/Faq';
+import Contact from './Contact/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './Product/Product';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Acceuil />} />
+        <Route path="boutique" element={<Boutique />} />
+        <Route path="boutique/produit/:productName" element={<Product />} />
+        <Route path="apropre" element={<Apropre />} />
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
