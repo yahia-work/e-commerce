07353c7e612e6d19d649086392fb981c8e9448ba
yahia-work <yahiawork35@gmail.com>
2024-10-11 13:15:36 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world i am yahia hanani
+        hello world i am othmani sadek
       </header>
     </div>
   );
